04a1dd06847e5ba75d8c033a899abefb0f34088e
itslu666 <philipp.j6y@gmail.com>
2024-11-15 00:23:29 +0100

    added style images

--- modules/get_buttons.py
+++ modules/get_buttons.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from PIL import Image
 
 
 def show_buttons(button_frame):
@@ -8,7 +9,14 @@ def show_buttons(button_frame):
 
             # create button with custom text for each style
             button = ctk.CTkButton(
-                button_frame, text=line.strip()
+                button_frame,
+                text=line.strip(),
+                image=ctk.CTkImage(
+                    Image.open(
+                        f"./data/images/{line.strip().replace(" ", "_").lower()}.png"
+                    ),
+                    size=(250, 250),
+                ),
             )  # click event to show next buttons and write into textbox and hide all buttons // image of that style
 
             button.pack(side="left", padx=10)
