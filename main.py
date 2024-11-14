75c6e0944e3e4ddcbb84420bae06f5d7ac14c0f4
itslu666 <philipp.j6y@gmail.com>
2024-11-14 22:01:59 +0100

    changed layout + added function to show style buttons

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import customtkinter as ctk
+from modules import get_buttons
+
 
 root = ctk.CTk()
 root.geometry("900x600")
 
 # make frames
-button_frame = ctk.CTkScrollableFrame(root, border_width=2, orientation="horizontal")
-prompt_frame = ctk.CTkFrame(root, border_width=2)
-bottom_frame = ctk.CTkFrame(root, border_width=2)
+# make a horizontal scrollable frame
+button_frame = ctk.CTkScrollableFrame(root, orientation="horizontal")
+prompt_frame = ctk.CTkFrame(root)
 
 # make prompt tb
 prompt_tb = ctk.CTkTextbox(prompt_frame)
 
-# place frames
-button_frame.place(relx=0, rely=0, relwidth=1, relheight=0.5)
-prompt_frame.place(relx=0, rely=0.5, relwidth=1, relheight=0.4)
-bottom_frame.place(relx=0, rely=0.9, relwidth=1, relheight=0.1) 
+# place frames + textbox
+button_frame.pack(expand=True, fill="both", pady=(10, 0), padx=10)
+prompt_frame.pack(expand=True, fill="both", pady=10, padx=10)
+prompt_tb.pack(expand=True, fill="both", padx=10, pady=10)
+
+# show all buttons / start the process
+get_buttons.show_buttons(button_frame)
 
 root.mainloop()
--- modules/get_buttons.py
+++ modules/get_buttons.py
@@ -1,1 +1,14 @@
 import customtkinter as ctk
+
+
+def show_buttons(button_frame):
+    # open file to get all styles
+    with open("./data/styles.txt") as file:
+        for line in file.readlines():
+
+            # create button with custom text for each style
+            button = ctk.CTkButton(
+                button_frame, text=line.strip()
+            )  # click event to show next buttons and hide all // image of that style
+
+            button.pack(side="left", padx=10)
