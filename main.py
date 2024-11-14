9e0215b4e0f532f8f241b884e3121a9edb3ffc2c
itslu666 <philipp.j6y@gmail.com>
2024-11-14 13:19:34 +0100

    add txt files + make ui layout

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import customtkinter as ctk
 
 root = ctk.CTk()
+root.geometry("900x600")
 
+# make frames
+button_frame = ctk.CTkScrollableFrame(root, border_width=2, orientation="horizontal")
+prompt_frame = ctk.CTkFrame(root, border_width=2)
+bottom_frame = ctk.CTkFrame(root, border_width=2)
+
+# make prompt tb
+prompt_tb = ctk.CTkTextbox(prompt_frame)
+
+# place frames
+button_frame.place(relx=0, rely=0, relwidth=1, relheight=0.5)
+prompt_frame.place(relx=0, rely=0.5, relwidth=1, relheight=0.4)
+bottom_frame.place(relx=0, rely=0.9, relwidth=1, relheight=0.1) 
 
 root.mainloop()
