9f39b503f61675dc70243cc50e82ecb17927057f
itslu666 <philipp.j6y@gmail.com>
2024-11-17 23:52:58 +0100

    format

--- modules/show_buttons.py
+++ modules/show_buttons.py
@@ -4,6 +4,8 @@ from modules import textbox_manager
 
 
 def style_buttons(button_frame, textbox):
+    state = "style"
+
     # open file to get all styles
     with open("./data/styles.txt") as file:
         for line in file.readlines():
@@ -11,7 +13,15 @@ def style_buttons(button_frame, textbox):
             # create button with custom text for each style
             button = ctk.CTkButton(
                 button_frame,
-                text=line.strip(), corner_radius=100, command=lambda line=line: textbox_manager.write_text(textbox, line), compound="top", fg_color="transparent", hover_color="grey", text_color="#F0F0F0" if ctk.get_appearance_mode() == "Dark" else "#2E2E2E",
+                text=line.strip(),
+                corner_radius=100,
+                command=lambda line=line: textbox_manager.write_text(textbox, line),
+                compound="top",
+                fg_color="transparent",
+                hover_color="grey",
+                text_color=(
+                    "#F0F0F0" if ctk.get_appearance_mode() == "Dark" else "#2E2E2E"
+                ),
                 image=ctk.CTkImage(
                     Image.open(
                         f"./data/images/{line.strip().replace(" ", "_").lower()}.png"
--- modules/textbox_manager.py
+++ modules/textbox_manager.py
@@ -1,5 +1,5 @@
 def write_text(textbox, text):
     textbox.insert("1.0", text + "\n")
 
-def copy_text():
-    ...
+
+def copy_text(): ...
